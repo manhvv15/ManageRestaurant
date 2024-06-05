0c1a13453a184a8f17c30842e1a39fb84e77631e
VU VAN MANH <manhvv15@gmail.com>
2024-06-05 23:52:09 +0700

    login

---
+++ ManageRestaurant/Controllers/Users/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageRestaurant.Controllers.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}
